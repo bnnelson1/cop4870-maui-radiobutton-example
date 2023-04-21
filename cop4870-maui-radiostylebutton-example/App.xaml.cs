9f5537b566c1528a2ebfdefb127f227df2cc9c35
Brian Nelson <122384363+bnnelson1@users.noreply.github.com>
2023-04-20 21:26:28 -0400

    Initial

---
+++ cop4870-maui-radiostylebutton-example/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace cop4870_maui_radiostylebutton_example;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ cop4870-maui-radiostylebutton-example/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using cop4870_maui_radiostylebutton_example.ViewModels;
+
+namespace cop4870_maui_radiostylebutton_example;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = new MainViewModel();
+	}
+}
