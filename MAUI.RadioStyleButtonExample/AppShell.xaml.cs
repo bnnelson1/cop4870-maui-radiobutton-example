ac1036a41b1e7d946bb3127ed5e0877cd803e5e1
Brian Nelson <122384363+bnnelson1@users.noreply.github.com>
2023-04-20 21:46:44 -0400

    Initial 3

--- MAUI.RadioStyleButtonExample/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace cop4870_maui_radiostylebutton_example;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- MAUI.RadioStyleButtonExample/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace cop4870_maui_radiostylebutton_example;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
